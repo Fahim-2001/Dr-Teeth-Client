df08334804f433e7abed9007a82d4997760e8345
unknown <mdyasirofficial2001@gmail.com>
2022-11-08 01:01:47 +0600

    first commit

---
+++ src/Routes/Routes.js
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+import Login from "../Pages/Login/Login";
+import Logout from "../Pages/Logout/Logout";
+import Services from "../Pages/Services/Services/Services";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+      {
+        path: "/services",
+        element: <Services></Services>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/logout",
+        element: <Logout></Logout>,
+      },
+    ],
+  },
+]);
+
+export default routes;
