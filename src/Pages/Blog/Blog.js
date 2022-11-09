bf3ea5c6fb1cb2b62554287974f2ce2b76c54b18
unknown <mdyasirofficial2001@gmail.com>
2022-11-09 11:51:24 +0600

    First Question Added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -3,7 +3,28 @@ import React from "react";
 const Blog = () => {
   return (
     <div>
-      <h1>Blog Section</h1>
+      {/* Question One */}
+      <div className="collapse mx-10">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl text-left font-medium">
+          Q1. What is the difference between SQL and NoSQL ?
+        </div>
+        <div className="collapse-content">
+          <p className="text-left">
+            The five critical differences between SQL vs NoSQL are: <br /> 1.SQL
+            databases are relational, NoSQL databases are non-relational. <br />{" "}
+            2.SQL databases use structured query language and have a predefined
+            schema. NoSQL databases have dynamic schemas for unstructured data.{" "}
+            <br />
+            3.SQL databases are vertically scalable, while NoSQL databases are
+            horizontally scalable. <br /> 4.SQL databases are table-based, while
+            NoSQL databases are document, key-value, graph, or wide-column
+            stores. <br /> 5.SQL databases are better for multi-row
+            transactions, while NoSQL is better for unstructured data like
+            documents or JSON.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
