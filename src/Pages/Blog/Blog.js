6810d6a9ce18eade51693f3b8d613bd378e3208e
unknown <mdyasirofficial2001@gmail.com>
2022-11-09 11:56:37 +0600

    Second Question added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -25,6 +25,28 @@ const Blog = () => {
           </p>
         </div>
       </div>
+      {/* Question Two */}
+      <div className="collapse mx-10">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl text-left font-medium">
+          Q2. What is JWT, and how does it work?
+        </div>
+        <div className="collapse-content">
+          <p className="text-left pr-1">
+            JSON Web Token (JWT) is an open standard (RFC 7519) for securely
+            transmitting information between parties as JSON object. It is
+            compact, readable and digitally signed using a private key/ or a
+            public key pair by the Identity Provider(IdP). So the integrity and
+            authenticity of the token can be verified by other parties involved.
+            The purpose of using JWT is not to hide data but to ensure the
+            authenticity of the data. JWT is signed and encoded, not encrypted.
+            JWT is a token based stateless authentication mechanism. Since it is
+            a client-side based stateless session, server doesn't have to
+            completely rely on a datastore(database) to save session
+            information.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
